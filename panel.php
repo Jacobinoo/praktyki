e9cd618a5b696120843fb1bf342d94a59132b732
Jacobino <69864916+Jacobinoo@users.noreply.github.com>
2021-11-30 12:09:33 +0100

    Add files via upload

--- panel.php
+++ panel.php
@@ -14,19 +14,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="stylesheet.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="scripts.js" defer></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
     </head>
-    <body>
+    <body id="panel">
         <div class="nav">
-            <div class="logo">Inteligentny Dom</div>
+            <div class="logo">Dom</div>
             <div class="separator"></div>
             <div class="links">
                 <a href="panel.php" class="link">Panel</a>
                 <a href="raspberry.php" class="link">Raspberry</a>
                 <a href="logout.php" class="link">Wyloguj się</a>
             </div>
+            <div class="account"><span class="material-icons" style="    font-size: 32px">account_circle</span></div>
         </div>
         <div class="devicesParent">
             <span class="devicesTitle">Akcesoria</span>
@@ -41,23 +41,142 @@
                     <div class="block_name">Lampka niebieska</div>
                     <div class="block_status" id="status">Wył.</div>
                 </div>
-                <div id="cam1" class="block" data-status="off">
-                    <div class="block_icon_div"><span class="material-icons" id="icon" style="font-size: 30px;">photo_camera</span></div>
-                    <div class="block_name" id="name">Kamera</div>
-                    <div class="block_status" id="status">Ukryte</div>
+                <div class="block" id="esp1" data-status="off">
+                    <div class="block_icon_div"><span class="material-icons" id="icon" style="font-size: 30px;">flash_on</span></div>
+                    <div class="block_name">ESP - dioda</div>
+                    <div class="block_status" id="status">Wył.</div>
+                </div>
+                <div class="block" id="buzz1" data-status="off">
+                    <div class="block_icon_div"><span class="material-icons" id="icon" style="font-size: 30px;">volume_up</span></div>
+                    <div class="block_name">Głośniczek</div>
+                    <div class="block_status" id="status">Wył.</div>
                 </div>
             </div>
         </div>
-
-            <!--<div class="panel">
-            <input type="number" class="input" id="port" placeholder="Wpisz numer portu"><br>
-            <div class="buttonParent">
-                <button id="on" class="button">On</button>
-                <button id="off" class="button">Off</button>
+        <div class="devicesParent">
+            <span class="devicesTitle">Kamery</span>
+            <div class="devices cameras">
             </div>
-            17-czerwona, 14 niebieska
-            </div>
-            <div class="panel"><img class="camera" id="camera" src="http://192.168.111.5:5000/video_feed"></div>
-        </div>-->
+        </div>
     </body>
+    <script>
+$(document).ready(function () {
+    $("#light1").click(function () {
+        if($(this).attr("data-status") == "off") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "17",
+                        action: "on"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "on") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "17",
+                        action: "off"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "off") {
+            $("#light1").attr("data-status", "on");
+            $('#light1 #status').text("Wł.");
+            $('#light1 #icon').css("color", "rgb(235, 181, 3)")
+        } else {
+            $("#light1").attr("data-status", "off");
+            $('#light1 #status').text("Wył.");
+            $('#light1 #icon').css("color", "rgb(170, 170, 170)")
+        }
+    });
+    $("#light2").click(function () {
+        if($(this).attr("data-status") == "off") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "14",
+                        action: "on"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "on") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "14",
+                        action: "off"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "off") {
+            $("#light2").attr("data-status", "on");
+            $('#light2 #status').text("Wł.");
+            $('#light2 #icon').css("color", "rgb(235, 181, 3)")
+        } else {
+            $("#light2").attr("data-status", "off");
+            $('#light2 #status').text("Wył.");
+            $('#light2 #icon').css("color", "rgb(170, 170, 170)")
+        }
+    });
+    $("#buzz1").click(function () {
+        if($(this).attr("data-status") == "off") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "16",
+                        action: "on"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "on") {
+            $.ajax({
+                type: "POST",
+                url: "control.php",
+                dataType: "json",
+                data: {port: "16",
+                        action: "off"
+                }
+            });
+        }
+        if($(this).attr("data-status") == "off") {
+            $("#buzz1").attr("data-status", "on");
+            $('#buzz1 #status').text("Wł.");
+            $('#buzz1 #icon').css("color", "rgb(66, 135, 245)")
+        } else {
+            $("#buzz1").attr("data-status", "off");
+            $('#buzz1 #status').text("Wył.");
+            $('#buzz1 #icon').css("color", "rgb(170, 170, 170)")
+        }
+    });
+    $("#esp1").click(function () {
+        if($(this).attr("data-status") == "off") {
+            $.ajax({
+                url: "http://192.168.111.28/LED=ON"
+            });
+        }
+        if($(this).attr("data-status") == "on") {
+            $.ajax({
+                url: "http://192.168.111.28/LED=OFF"
+            });
+        }
+        if($(this).attr("data-status") == "off") {
+            $("#esp1").attr("data-status", "on");
+            $('#esp1 #status').text("Wł.");
+            $('#esp1 #icon').css("color", "rgb(235, 181, 3)")
+        } else {
+            $("#esp1").attr("data-status", "off");
+            $('#esp1 #status').text("Wył.");
+            $('#esp1 #icon').css("color", "rgb(170, 170, 170)")
+        }
+    });
+    //Kamery
+    $(".cameras").append("<a target='_blank' href='http://<?=$_SERVER['SERVER_ADDR']?>:5000'><img class='camera_block' src='http://<?=$_SERVER['SERVER_ADDR']?>:5000/video_feed'></a>");
+});
+</script>
 </html>
